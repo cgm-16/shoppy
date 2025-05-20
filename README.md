ffb44f97ca1ffdca90e46151d6a550d4adac68d0
Gwangmin Choi <sdgwsld@gmail.com>
2025-05-20 14:33:40 +0900

    [UI] updated logo and md file, fixed a bit of code

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# A shopping app made with React and React Query
-### [Link](https://shopping-module.vercel.app/deals/time-deal)
+# Shopping Module
+
+#### [서비스 링크](https://shopping-module.vercel.app)
+
+## 간단 설명
+- 쇼핑몰 클론 프로젝트
+- 무한 스크롤 구현, 타이머 기능 개발
+
+## 기술 스택
+- 라이브러리: React, Tanstack Query
+- 빌드 도구: vite
--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,7 @@ import { router } from "./router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 100000,
-      },
-    },
-  });
+  const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
